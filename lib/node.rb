d2b0efff280f876e695bcc58d0c1f7acb1ff9bc5
Bradley J. Tannor <bradleytannor@gmail.com>
2020-03-12 09:34:52 -0700

    Integrated
    Integrated the interface and implementation components.

--- lib/node.rb
+++ lib/node.rb
@@ -2,10 +2,12 @@
 # under the MIT License.
 
 require "node/version"
+require 'node_impl'
 
 # Node.
 # @abstract
-# $DESCRIPTION$.
-module Node
+# A doubly-linked Node class. The interface contains the method signatures
+# and the constants. The implementation contains the method implementations.
+module NodeLibrary
 end
 
--- lib/node/version.rb
+++ lib/node/version.rb
@@ -1,6 +1,10 @@
 # Copyright (C) 2020 Diligent Software LLC. All rights reserved. Released under
 # the MIT License.
 
-module Node
+# Node.
+# @abstract
+# A doubly-linked Node class. The interface contains the method signatures
+# and the constants. The implementation contains the method implementations.
+module NodeLibrary
   VERSION = ::VERSION
 end
